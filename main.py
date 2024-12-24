45968eea69e3c5738108104aa02f88967fe7928b
dani_falex <dani_falex@hotmail.com>
2024-12-23 22:26:42 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("jornada de dados")
+
+# variaveis
+idade = 34
+nome = "Daniela"
+
+# print
+
+print(idade)
+
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [0,1,2,3,4,5]
+
+# métodos
+lista.reverse()
+print("lista invertida: ", lista)
